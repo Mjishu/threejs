9a7a39a8c9bdd2fd99b5d176f9f98341e82256e3
mjishu <128941980+Mjishu@users.noreply.github.com>
2024-10-13 11:47:57 -0400

    working through three.js docs tutorials

---
+++ tutorial/main.js
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
+if (!WebGL.isWebGL2Available()) {
+    WebGL.getWebGL2ErrorMessage()
+    document.getElementById("container").appendChild(warning)
+}
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//*   Box
+// const Boxgeometry = new THREE.BoxGeometry(1, 1, 1);
+// const Boxmaterial = new THREE.MeshBasicMaterial({ color: 0x60ffcc });
+// const cube = new THREE.Mesh(Boxgeometry, Boxmaterial);
+// scene.add(cube);
+// camera.position.z = 5;
+
+// * Line
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffccf0 });
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(lineGeometry, lineMaterial)
+scene.add(line);
+
+renderer.render(scene, camera)
+
+// function animate() {
+//     requestAnimationFrame(animate)
+//     renderer.render(scene, camera);
+// }
+// renderer.setAnimationLoop(animate);
